95bc1b4f46cb81571ae7a8f79d2eb428920d72d6
Thilan Hasitha <156594121+thilanhasitha@users.noreply.github.com>
2025-08-29 16:30:50 +0530

    prompt.py file updated newly

---
+++ src/prompt.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+system_prompt =(
+    "you are assistant for the question-answering task "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer,say that you "
+    "don't know. Use three sentences maximum  and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
