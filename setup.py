ed029eb3bb9f8b8516e8077ed6fa866444cd0b9b
Thilan Hasitha <156594121+thilanhasitha@users.noreply.github.com>
2025-08-29 12:54:46 +0530

    setup.py file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Genarative AI Project",
+    version = "0.0.0",
+    auther = "thilan hasitha",
+    auther_email = "yasanjithbgth@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+
+)
